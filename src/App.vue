579107450c5bf769a960e43f9d43a3494e0c30ad
Petr <lithx@yandex.com>
2017-11-06 17:08:39 +0300

    Writing search bar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <SearchBar class="app__search" />
+    <BookmarkList />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-};
+  import SearchBar from './components/SearchBar';
+  import BookmarkList from './components/BookmarkList';
+
+  export default {
+    name: 'app',
+    components: { SearchBar, BookmarkList },
+  };
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "../node_modules/divine.css/src/divine.css";
+  @import "variables.css";
+  @import "fonts.css";
+
+  body {
+    font-family: 'Roboto', sans-serif;
+
+    background-color: #000;
+  }
+
+  body:before {
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    width: 100vw;
+    height: 100vh;
+
+    content: '';
+
+    background-repeat: no-repeat no-repeat;
+    background-position: center;
+    background-size: cover;
+    background-image: url(https://i.imgur.com/Lsnc2yC.jpg);
+    opacity: 0.5;
+    transition: opacity 1s ease-in-out;
+
+    will-change: transform;
+
+    z-index: -1;
+  }
+
+  .app__search {
+    margin: var(--search-bar-margin);
+  }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <input class="search-bar__input-field" type="search" title="Search" placeholder="Filter bookmarks or press Enter to search Google">
+</template>
+
+<script>
+  export default {
+    name: 'SearchBar',
+  };
+</script>
+
+<style scoped>
+  .search-bar__input-field {
+    padding: .5rem;
+    border-radius: 4px;
+    background-color: rgba(255,255,255,.3);
+    width: calc(100% - (var(--search-bar-margin) * 2));
+    transition: .5s background-color;
+  }
+
+  .search-bar__input-field:hover {
+    background-color: rgba(255,255,255,.4);
+    transition: .5s background-color;
+  }
+
+  .search-bar__input-field:focus,
+  .search-bar__input-field:active {
+    background-color: rgba(255,255,255,.6);
+    transition: .5s background-color;
+  }
+
+  .search-bar__input-field::placeholder {
+    color: rgba(255,255,255,.6);
+    transition: .5s color;
+  }
+
+  .search-bar__input-field:focus::placeholder,
+  .search-bar__input-field:active::placeholder {
+    color: transparent;
+    transition: .5s color;
+  }
+</style>
